2bda84d25b0f1ed217e5dfae0fd5a6f0b390d44b
Nikhil Makhijani <72851103+nikhilmakhijani@users.noreply.github.com>
2023-03-09 16:50:02 +0530

    Update te.tf

--- te.tf
+++
@@ -1,3 +0,0 @@
-
-  Great job on the GCP platform project! Your expertise in Terraform and problem-solving skills have been invaluable to the success of the project. Your ability to troubleshoot and find solutions for issues has saved a lot of time and effort for the team, and your contributions have been greatly appreciated.
-
