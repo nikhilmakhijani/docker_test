a9cf5a273469d83faf74f974d46c67312dd7c4a5
Nikhil Makhijani <72851103+nikhilmakhijani@users.noreply.github.com>
2023-05-18 14:40:00 +0530

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Substitute environment variables in the entrypoint script
-envsubst < /entry.template.sh > /entry.sh
+envsubst "\$MIGRATION_PATH" < /entry.template.sh > /entry.sh
+envsubst "\$$DB_USER" < /entry.template.sh > /entry.sh
+envsubst "\$$DB_PASS" < /entry.template.sh > /entry.sh
+envsubst "\$$DB_HOST" < /entry.template.sh > /entry.sh
+envsubst "\$$DB_NAME" < /entry.template.sh > /entry.sh
 
 # Make the entrypoint script executable
 chmod +x /entry.sh
