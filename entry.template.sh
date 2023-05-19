e67e46274aff8765f589a1c08cbc089e20d6c578
Nikhil Makhijani <72851103+nikhilmakhijani@users.noreply.github.com>
2023-05-19 10:57:54 +0530

    Update entry.template.sh

--- entry.template.sh
+++ entry.template.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-# Set the environment variables
-export MIGRATION_PATH="$MIGRATION_PATH"
-export DB_USER="$DB_USER"
-export DB_PASS="$DB_PASS"
-export DB_HOST="$DB_HOST"
-export DB_NAME="$DB_NAME"
-
-# Execute the migration command
-migrate -path "$MIGRATION_PATH" -database "postgres://$DB_USER:$DB_PASS@$DB_HOST/$DB_NAME" up
+./cloud-sql-proxy --private-ip engaged-hook-384215:us-central1:test &
+sleep 15
+migrate -path ${MIGRATION_PATH} -database postgres://${DB_USER}:${DB_PASS}@127.0.0.1:5432/${DB_NAME}?sslmode=disable up
+sleep 5
