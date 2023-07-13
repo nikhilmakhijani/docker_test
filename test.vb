ad70f6ab2059d441c578f1d6793feed993635504
Nikhil Makhijani <72851103+nikhilmakhijani@users.noreply.github.com>
2023-07-13 11:58:53 +0530

    Update test.vb

--- test.vb
+++ test.vb
@@ -1,19 +1,19 @@
-Sub LookupAndCopyColumns()
-    ' Source file path (current workbook)
+Sub LookupOneDriveAndCopyColumnsToLocal()
+    ' Source file path (OneDrive)
     Dim sourceFilePath As String
-    sourceFilePath = ThisWorkbook.FullName
+    sourceFilePath = "https://onedrive.live.com/.../SourceFile.xlsx"
     
     ' Source sheet name
     Dim sourceSheetName As String
     sourceSheetName = "Sheet1"
     
-    ' Lookup range in the source sheet
+    ' Lookup range in the current workbook
     Dim lookupRange As Range
-    Set lookupRange = ThisWorkbook.Sheets(sourceSheetName).Range("A1:A10") ' Adjust the range as per your requirement
+    Set lookupRange = ThisWorkbook.Sheets("Sheet1").Range("A1:A10") ' Adjust the range as per your requirement
     
-    ' Destination file path (OneDrive)
+    ' Destination file path (local)
     Dim destinationFilePath As String
-    destinationFilePath = "https://onedrive.live.com/.../DestinationFile.xlsx"
+    destinationFilePath = "C:\DestinationFile.xlsx"
     
     ' Destination sheet name
     Dim destinationSheetName As String
@@ -23,7 +23,7 @@ Sub LookupAndCopyColumns()
     Dim excelApp As Object
     Set excelApp = CreateObject("Excel.Application")
     
-    ' Open the source file
+    ' Open the source file (OneDrive)
     Dim sourceWorkbook As Object
     Set sourceWorkbook = excelApp.Workbooks.Open(sourceFilePath)
     
@@ -31,7 +31,7 @@ Sub LookupAndCopyColumns()
     Dim sourceSheet As Object
     Set sourceSheet = sourceWorkbook.Sheets(sourceSheetName)
     
-    ' Get the destination workbook
+    ' Get the destination workbook (local)
     Dim destinationWorkbook As Object
     Set destinationWorkbook = excelApp.Workbooks.Open(destinationFilePath)
     
@@ -51,30 +51,20 @@ Sub LookupAndCopyColumns()
         
         ' If a matching row is found, copy the corresponding columns to the destination sheet
         If Not rowToCopy Is Nothing Then
-            rowToCopy.EntireRow.Copy
-            destinationSheet.Cells(lookupCell.Row, destinationColumnIndex).PasteSpecial Paste:=xlPasteValues
+            Dim sourceData As Range
+            Set sourceData = rowToCopy.EntireRow.Resize(, 2) ' Adjust the number of columns to copy
+            
+            Dim destinationCell As Range
+            Set destinationCell = destinationSheet.Cells(lookupCell.Row, destinationColumnIndex)
+            
+            destinationCell.Resize(, 2).Value = sourceData.Value ' Adjust the number of columns to paste
         End If
     Next lookupCell
     
-    ' Save and close the destination workbook
+    ' Save and close the destination workbook (local)
     destinationWorkbook.Save
     destinationWorkbook.Close
     
-    ' Close the source workbook without saving
+    ' Close the source workbook (OneDrive) without saving
     sourceWorkbook.Close False
     
-    ' Close Excel
-    excelApp.Quit
-    
-    ' Release the objects from memory
-    Set lookupRange = Nothing
-    Set rowToCopy = Nothing
-    Set sourceSheet = Nothing
-    Set sourceWorkbook = Nothing
-    Set destinationSheet = Nothing
-    Set destinationWorkbook = Nothing
-    Set excelApp = Nothing
-    
-    ' Display a message
-    MsgBox "Data copied successfully!"
-End Sub
